aa492b486cd6419eed78eec475dfb5b2cec2e477
Zafer Balkan <zafer@zaferbalkan.com>
2026-04-16 16:22:43 +0300

    Refactor adapter discovery to async UI flow and lazy WMI

--- src/MacChanger.Gui/Forms/MainForm.cs
+++ src/MacChanger.Gui/Forms/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MacChanger.Gui.DTO;
@@ -12,15 +13,68 @@ namespace MacChanger.Gui.Forms
     public partial class MainForm : Form
     {
         internal List<NetworkConnection> NetworkConnections { get; set; }
+
         private const string zeroMacValue = "00-00-00-00-00-00";
+
         private readonly VendorManager _vm;
+        private readonly Panel _loadingPanel;
+        private readonly ProgressBar _loadingProgressBar;
+        private readonly Label _loadingLabel;
+
+        private CancellationTokenSource _refreshCancellation;
+        private bool _isRefreshing;
+
         private bool _locallyAdministered;
         private bool _reenableOnChange;
         private NetworkConnectionDetail _selected;
+
         public MainForm()
         {
             _vm = new VendorManager();
             InitializeComponent();
+
+            _loadingProgressBar = new ProgressBar
+            {
+                Style = ProgressBarStyle.Marquee,
+                MarqueeAnimationSpeed = 20,
+                Width = 250,
+                Height = 18,
+                Anchor = AnchorStyles.None
+            };
+
+            _loadingLabel = new Label
+            {
+                AutoSize = true,
+                Anchor = AnchorStyles.None,
+                Text = "Loading network adapters..."
+            };
+
+            var loadingLayout = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                ColumnCount = 1,
+                RowCount = 3,
+                BackColor = Color.FromArgb(225, SystemColors.Control)
+            };
+            loadingLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f));
+            loadingLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 45f));
+            loadingLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            loadingLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            loadingLayout.Controls.Add(_loadingLabel, 0, 1);
+            loadingLayout.Controls.Add(_loadingProgressBar, 0, 2);
+
+            _loadingPanel = new Panel
+            {
+                Dock = DockStyle.Fill,
+                Visible = false
+            };
+            _loadingPanel.Controls.Add(loadingLayout);
+
+            MainTableLayoutPanel.Controls.Add(_loadingPanel, 0, 0);
+            _loadingPanel.BringToFront();
+
+            ConnectionsGrid.EmptyListMsg = "No network adapters loaded.";
+            UpdateSelectionState();
         }
 
         #region EventHandlers
@@ -33,15 +87,23 @@ namespace MacChanger.Gui.Forms
 
         private void ChangeMacButton_Click(object sender, EventArgs e)
         {
+            if (_selected == null)
+            {
+                return;
+            }
+
             var targetMac = macTextBox.Text;
 
             // Ignore default value to prevend accidents
-            if (targetMac.Equals(zeroMacValue)) return;
+            if (targetMac.Equals(zeroMacValue))
+            {
+                return;
+            }
 
             if (_selected.TryUpdateMac(new MacAddress(targetMac)))
             {
                 _ = MessageBox.Show("Successfully updated MAC address", "MAC Address Change", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                RefreshConnectionsBackground();
+                _ = RefreshConnectionsBackground();
             }
             else
             {
@@ -55,6 +117,11 @@ namespace MacChanger.Gui.Forms
 
         private void ConnectionsGrid_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (_isRefreshing)
+            {
+                return;
+            }
+
             if (ConnectionsGrid?.SelectedItem != null)
             {
                 // The event is triggered twice:
@@ -63,21 +130,14 @@ namespace MacChanger.Gui.Forms
                 if (ConnectionsGrid.SelectedItem.Index != -1)
                 {
                     var row = ConnectionsGrid.SelectedItem.RowObject as NetworkConnection;
-                    _selected = row.Detail;
-                    ConnectionValueTextbox.Text = _selected.Name;
-                    DeviceValueTextbox.Text = _selected.Device;
-                    HardwareIdValueTextbox.Text = _selected.HardwareId;
-                    ConfigIdValueTextbox.Text = _selected.ConfigId;
-                    Ipv4ValueTextbox.Text = _selected.IPv4Status;
-                    Ipv6ValueTextbox.Text = _selected.IPv6Status;
-                    OriginalMacValueTextbox.Text = _selected.OriginalMac;
-                    OriginalMacVendorTextbox.Text = _selected.OriginalVendor;
-                    ActiveMacValueTextbox.Text = _selected.ActiveMac;
-                    ActiveMacVendorTextbox.Text = _selected.ActiveVendor;
-                    DhcpEnabledItem.Checked = _selected.IsDhcpEnabled;
-                    DhcpRenewIpItem.Enabled = _selected.IsDhcpEnabled;
-                    DhcpReleaseIpItem.Enabled = _selected.IsDhcpEnabled;
+                    _selected = row?.Detail;
+                    BindSelection();
                 }
+            }
+            else
+            {
+                _selected = null;
+                BindSelection();
             }
         }
 
@@ -85,6 +145,11 @@ namespace MacChanger.Gui.Forms
 
         private void Dhcp4EnabledItem_Click(object sender, EventArgs e)
         {
+            if (_selected == null)
+            {
+                return;
+            }
+
             if (DhcpEnabledItem.Checked)
             {
                 if (_selected.TryDhcpDisable())
@@ -99,11 +164,17 @@ namespace MacChanger.Gui.Forms
                     DhcpEnabledItem.Checked = true;
                 }
             }
-            RefreshConnectionsBackground();
+
+            _ = RefreshConnectionsBackground();
         }
 
         private void DhcpReleaseIpItem_Click(object sender, EventArgs e)
         {
+            if (_selected == null)
+            {
+                return;
+            }
+
             if (_selected.TryDhcpRelease(out var message))
             {
                 MessageBox.Show(message, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -116,6 +187,11 @@ namespace MacChanger.Gui.Forms
 
         private void DhcpRenewIpItem_Click(object sender, EventArgs e)
         {
+            if (_selected == null)
+            {
+                return;
+            }
+
             if (_selected.TryDhcpRenew(out var message))
             {
                 MessageBox.Show(message, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -138,7 +214,10 @@ namespace MacChanger.Gui.Forms
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            _refreshCancellation?.Cancel();
+            _refreshCancellation?.Dispose();
             _selected?.Dispose();
+            DisposeConnections(NetworkConnections);
             _vm?.Dispose();
         }
 
@@ -146,11 +225,9 @@ namespace MacChanger.Gui.Forms
         {
             MakeTextboxBackgroundTransparent();
 
-            await RefreshConnectionsBackground();
+            await RefreshConnectionsBackground(clearListWhileLoading: true);
             ConnectionsGrid.SelectedItem = null;
 
-            
-            // Fill in vendor combobox
             VendorComboBox.DataSource = await Task.Run(() => _vm.GetVendorList().ToList());
             VendorComboBox.SelectedItem = null;
         }
@@ -179,6 +256,11 @@ namespace MacChanger.Gui.Forms
 
         private void RandomMacButton_Click(object sender, EventArgs e)
         {
+            if (_selected == null)
+            {
+                return;
+            }
+
             var randomVendor = _vm.GetRandom();
             var randomMac = _selected.GetRandom(randomVendor.Oui);
 
@@ -188,19 +270,27 @@ namespace MacChanger.Gui.Forms
             {
                 randomMac.SetAsLocallyAdministered();
             }
+
             macTextBox.Text = randomMac.ToString(MacAddress.MacDelimiter.Colon);
         }
 
         private async void RefreshItem_Click(object sender, EventArgs e)
         {
-            MainStatusBar.Text = "Refreshing network interface data...";
             await RefreshConnectionsBackground();
-            MainStatusBar.Text = "Ready";
         }
 
         private void RestoreMacButton_Click(object sender, EventArgs e)
         {
-            if (_selected.Changed == "No") { return; }
+            if (_selected == null)
+            {
+                return;
+            }
+
+            if (_selected.Changed == "No")
+            {
+                return;
+            }
+
             if (_selected.TryReset())
             {
                 _ = MessageBox.Show("Successfully restored MAC address", "MAC Address Restore", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -209,7 +299,8 @@ namespace MacChanger.Gui.Forms
                 {
                     _ = _selected.TryEnable();
                 }
-                RefreshConnectionsBackground();
+
+                _ = RefreshConnectionsBackground();
             }
             else
             {
@@ -220,6 +311,7 @@ namespace MacChanger.Gui.Forms
         private void SavePresetAsItem_Click(object sender, EventArgs e) => NotImplemented();
 
         private void SavePresetItem_Click(object sender, EventArgs e) => NotImplemented();
+
         private async void UpdateOuiItem_ClickAsync(object sender, EventArgs e)
         {
             var result = MessageBox.Show("This will initiate OUI download in the background.\n\nAre you sure?", "Update Vendor List (OUI) from IEEE", MessageBoxButtons.YesNo);
@@ -237,24 +329,16 @@ namespace MacChanger.Gui.Forms
 
         private void WikiLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            // Specify that the link was visited.
             WikiLink.LinkVisited = true;
-
-            // Navigate to a URL.
             VisitUrl("https://en.wikipedia.org/wiki/MAC_address#IEEE_802c_local_MAC_address_usage");
         }
 
         private void ZeroTwoCheckBox_CheckedChanged(object sender, EventArgs e) => _locallyAdministered = ZeroTwoCheckBox.Checked;
+
         #endregion EventHandlers
 
         #region Private Methods
 
-        private void SetUserFonts(float scaleFactorX, float scaleFactorY)
-        {
-            var OldFont = Font;
-            Font = new Font(OldFont.FontFamily, 11f * scaleFactorX, OldFont.Style, GraphicsUnit.Pixel);
-            OldFont.Dispose();
-        }
 
         /// <summary>
         ///     A placeholder method for events not implemented.
@@ -273,16 +357,183 @@ namespace MacChanger.Gui.Forms
             }
         }
 
-        private void RefreshConnections()
+        private void BindSelection()
         {
-            ConnectionsGrid.BeginUpdate();
-            NetworkConnections = NetworkAdapterFactory.GetNetworkAdapters(_vm).Select(adapter => new NetworkConnection(adapter)).ToList();
-            ConnectionsGrid.DataSource = NetworkConnections;
-            ConnectionsGrid.EndUpdate();
-            ConnectionsGrid.AutoResizeColumns();
+            var hasSelection = _selected != null;
+            if (!hasSelection)
+            {
+                ConnectionValueTextbox.Text = "...";
+                DeviceValueTextbox.Text = "...";
+                HardwareIdValueTextbox.Text = "...";
+                ConfigIdValueTextbox.Text = "...";
+                Ipv4ValueTextbox.Text = "...";
+                Ipv6ValueTextbox.Text = "...";
+                OriginalMacValueTextbox.Text = "...";
+                OriginalMacVendorTextbox.Text = "...";
+                ActiveMacValueTextbox.Text = "...";
+                ActiveMacVendorTextbox.Text = "...";
+                DhcpEnabledItem.Checked = false;
+                UpdateSelectionState();
+                return;
+            }
+
+            ConnectionValueTextbox.Text = _selected.Name;
+            DeviceValueTextbox.Text = _selected.Device;
+            HardwareIdValueTextbox.Text = _selected.HardwareId;
+            ConfigIdValueTextbox.Text = _selected.ConfigId;
+            Ipv4ValueTextbox.Text = _selected.IPv4Status;
+            Ipv6ValueTextbox.Text = _selected.IPv6Status;
+            OriginalMacValueTextbox.Text = _selected.OriginalMac;
+            OriginalMacVendorTextbox.Text = _selected.OriginalVendor;
+            ActiveMacValueTextbox.Text = _selected.ActiveMac;
+            ActiveMacVendorTextbox.Text = _selected.ActiveVendor;
+            DhcpEnabledItem.Checked = _selected.IsDhcpEnabled;
+            UpdateSelectionState();
         }
 
-        private Task RefreshConnectionsBackground() => Task.Factory.StartNew(() => Invoke(new Action(RefreshConnections)));
+        private void UpdateSelectionState()
+        {
+            var hasSelection = _selected != null;
+            var enableActions = hasSelection && !_isRefreshing;
+
+            ChangeMacButton.Enabled = enableActions;
+            RestoreMacButton.Enabled = enableActions;
+            RandomMacButton.Enabled = enableActions;
+            DhcpEnabledItem.Enabled = enableActions;
+            DhcpRenewIpItem.Enabled = enableActions && _selected != null && _selected.IsDhcpEnabled;
+            DhcpReleaseIpItem.Enabled = enableActions && _selected != null && _selected.IsDhcpEnabled;
+            DeleteItem.Enabled = enableActions;
+        }
+
+        private void SetLoadingState(bool isLoading, bool clearListWhileLoading)
+        {
+            _isRefreshing = isLoading;
+            RefreshItem.Enabled = !isLoading;
+            ConnectionsGrid.Enabled = !isLoading;
+
+            if (isLoading)
+            {
+                MainStatusBar.Text = "Loading network adapters...";
+                _loadingLabel.Text = "Loading network adapters...";
+                _loadingPanel.Visible = true;
+                _loadingPanel.BringToFront();
+
+                if (clearListWhileLoading)
+                {
+                    ConnectionsGrid.EmptyListMsg = "Loading network adapters...";
+                    ConnectionsGrid.DataSource = Array.Empty<NetworkConnection>();
+                    _selected = null;
+                    BindSelection();
+                }
+            }
+            else
+            {
+                _loadingPanel.Visible = false;
+                ConnectionsGrid.EmptyListMsg = "No network adapters loaded.";
+            }
+
+            UpdateSelectionState();
+        }
+
+        private async Task RefreshConnectionsBackground(bool clearListWhileLoading = false)
+        {
+            if (_isRefreshing)
+            {
+                return;
+            }
+
+            _refreshCancellation?.Cancel();
+            _refreshCancellation?.Dispose();
+            _refreshCancellation = new CancellationTokenSource();
+            var cancellationToken = _refreshCancellation.Token;
+
+            SetLoadingState(true, clearListWhileLoading);
+
+            try
+            {
+                var updatedConnections = await Task.Run(() =>
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    var adapters = NetworkAdapterFactory.GetNetworkAdapters(_vm).ToList();
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    return adapters.Select(adapter => new NetworkConnection(adapter)).ToList();
+                }, cancellationToken);
+
+                if (cancellationToken.IsCancellationRequested || IsDisposed)
+                {
+                    DisposeConnections(updatedConnections);
+                    return;
+                }
+
+                BeginInvoke(new Action(() =>
+                {
+                    if (IsDisposed)
+                    {
+                        DisposeConnections(updatedConnections);
+                        return;
+                    }
+
+                    ConnectionsGrid.BeginUpdate();
+                    DisposeConnections(NetworkConnections);
+                    NetworkConnections = updatedConnections;
+                    ConnectionsGrid.DataSource = NetworkConnections;
+                    ConnectionsGrid.EndUpdate();
+                    ConnectionsGrid.AutoResizeColumns();
+
+                    _selected = null;
+                    BindSelection();
+                    MainStatusBar.Text = $"Loaded {NetworkConnections.Count} adapters.";
+                }));
+            }
+            catch (OperationCanceledException)
+            {
+                if (!IsDisposed)
+                {
+                    MainStatusBar.Text = "Adapter loading cancelled.";
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!IsDisposed)
+                {
+                    MainStatusBar.Text = "Failed to load network adapters.";
+                    _ = MessageBox.Show(ex.Message, "Adapter Discovery Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            finally
+            {
+                if (!IsDisposed)
+                {
+                    BeginInvoke(new Action(() =>
+                    {
+                        if (!IsDisposed)
+                        {
+                            SetLoadingState(false, false);
+                            if (MainStatusBar.Text == "Loading network adapters...")
+                            {
+                                MainStatusBar.Text = "Ready";
+                            }
+                        }
+                    }));
+                }
+            }
+        }
+
+        private static void DisposeConnections(List<NetworkConnection> connections)
+        {
+            if (connections == null)
+            {
+                return;
+            }
+
+            foreach (var networkConnection in connections)
+            {
+                networkConnection.Dispose();
+            }
+        }
+
         #endregion Private Methods
     }
 }
