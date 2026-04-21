71ae0278a1ef07de2ceec25896faeb7023884220
Zafer Balkan <zafer@zaferbalkan.com>
2026-04-21 12:31:40 +0300

    Updated Assembly Info

--- src/DZMAC/Properties/AssemblyInfo.cs
+++ src/DZMAC/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DZMAC")]
+[assembly: AssemblyTitle("Dzmac.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DeltaZulu OÜ")]
 [assembly: AssemblyProduct("DZMAC")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
