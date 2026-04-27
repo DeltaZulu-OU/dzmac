5b301bb052d5966455e2c0568f9a790097370541
Zafer Balkan <zafer@zaferbalkan.com>
2026-04-27 13:12:36 +0300

    Added tests for some CLI parameters

---
+++ src/DZMAC.Tests/CliHandlerTests.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+
+namespace Dzmac.Tests
+{
+    [TestClass]
+    public class CliHandlerTests
+    {
+        private static readonly MethodInfo TryParseMethod = typeof(CliHandler).GetMethod(
+            "TryParse",
+            BindingFlags.NonPublic | BindingFlags.Static) ?? throw new InvalidOperationException("CliHandler.TryParse not found.");
+
+        [TestMethod]
+        public void TryParseShouldRejectMissingConnectionNameValueWhenNextTokenIsAnotherOption()
+        {
+            var args = new object[] { new[] { "-n", "-m" }, null, null };
+
+            var parsed = (bool)TryParseMethod.Invoke(null, args)!;
+
+            Assert.IsFalse(parsed);
+            Assert.AreEqual("Missing value for option '-n'.", (string)args[2]!);
+        }
+
+        [TestMethod]
+        public void TryParseShouldRejectMissingIpv4ValueWhenNextTokenIsAnotherOption()
+        {
+            var args = new object[] { new[] { "-i", "-g", "10.0.0.1/1" }, null, null };
+
+            var parsed = (bool)TryParseMethod.Invoke(null, args)!;
+
+            Assert.IsFalse(parsed);
+            Assert.AreEqual("Missing value for option '-i'.", (string)args[2]!);
+        }
+    }
+}
